82bea816e410e253083ae4f9369e0e543888dc9a
Shantel <Shantelgray24@gmail.com>
2025-05-14 08:12:04 -0400

    fixed my syntax and use prettier format

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials.map(word => 
-  word.split(' ')
-  .map(word => word[0].toUpperCase() + word.slice(1)
-  )
-  .join(' ')
-);
+  return tutorials.map((word) =>
+    word
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ")
+  );
 };
